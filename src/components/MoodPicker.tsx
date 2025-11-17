60a979ffc2194355fdf259ecf013ae7aa7f18231
Alexandr <alex@MacBook-Air-Alexandr.local>
2025-11-17 12:39:12 +0300

    Стилизовал MoodPicker. Вынес часть стилизации в отдельный файл theme

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 // import _ from 'lodash';
 //я бы хотел использовать ... <Text key={_.uniqueId("option.emoji_")} style={styles.moodText}> ...
 import { MoodOptionType } from '../types.ts';
+import { theme } from '../theme.ts';
 
 const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
@@ -16,26 +17,31 @@ const moodOptions: MoodOptionType[] = [
 export const MoodPicker: React.FC = () => {
   const [selectedMood, setSelectedMood] = useState<MoodOptionType>();
   return (
-    <View style={styles.moodList}>
-      {moodOptions.map(option => (
-        <View>
-          <Pressable
-            onPress={() => setSelectedMood(option)}
-            key={option.emoji}
-            style={[
-              styles.moodItem,
-              option.emoji === selectedMood?.emoji
-                ? styles.selectedMoodItem
-                : undefined,
-            ]}
-          >
-            <Text style={styles.moodText}>{option.emoji}</Text>
-          </Pressable>
-          <Text style={styles.descriptionText}>
-            {selectedMood?.emoji === option.emoji ? option.description : ' '}
-          </Text>
-        </View>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.heading}>How are you right now?</Text>
+      <View style={styles.moodList}>
+        {moodOptions.map(option => (
+          <View key={option.emoji}>
+            <Pressable
+              onPress={() => setSelectedMood(option)}
+              style={[
+                styles.moodItem,
+                option.emoji === selectedMood?.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}
+            >
+              <Text style={styles.moodText}>{option.emoji}</Text>
+            </Pressable>
+            <Text style={styles.descriptionText}>
+              {selectedMood?.emoji === option.emoji ? option.description : ' '}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
     </View>
   );
 };
@@ -47,7 +53,6 @@ const styles = StyleSheet.create({
   moodList: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 20,
   },
   moodItem: {
     width: 60,
@@ -59,13 +64,40 @@ const styles = StyleSheet.create({
   },
   selectedMoodItem: {
     borderWidth: 2,
-    backgroundColor: '#454C73',
-    borderColor: '#fff',
+    backgroundColor: theme.colorPurple,
+    borderColor: theme.colorWhite,
   },
   descriptionText: {
-    color: '#454C73',
+    color: theme.colorPurple,
     fontWeight: 'bold',
     fontSize: 10,
     textAlign: 'center',
+  },
+  container: {
+    borderWidth: 2,
+    borderColor: theme.colorPurple,
+    margin: 10,
+    borderRadius: 10,
+    padding: 20,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: theme.colorPurple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
