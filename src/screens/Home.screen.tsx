74c9d8916e7def8d6db6814e7b56988b0ded3d7c
Alexandr <alex@MacBook-Air-Alexandr.local>
2025-11-19 16:59:30 +0300

    Лог настроения поместил в компонент MoodItemRow. Установил билиотеку date-fns

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState, useCallback } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { MoodOptionType, MoodOptionWithTimestamp } from '../types.ts';
 import { MoodPicker } from '../components/MoodPicker';
-import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
+import { MoodItemRow } from '../components/MoodItemRow.tsx';
 
 export const Home: React.FC = () => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
@@ -15,9 +16,7 @@ export const Home: React.FC = () => {
     <View style={styles.container}>
       <MoodPicker onSelect={handleSelectMood} />
       {moodList.map(item => (
-        <Text key={item.timestamp}>
-          {item.mood.emoji} {new Date(item.timestamp).toString()}
-        </Text>
+        <MoodItemRow item={item} key={item.timestamp} />
       ))}
     </View>
   );
@@ -29,3 +28,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// мне не нравится порядок лога- новые записи появляются в внизу списка, а не вверху
+// Есть сомнения в эффективности useCallback, так как у меня перерисовываются все компоненты при любом взаимодействии.
