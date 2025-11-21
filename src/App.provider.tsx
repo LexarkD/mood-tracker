b731719cb9f1f9ccbcd9da6bb8dfde7f32ddffcb
Alexandr <alex@MacBook-Air-Alexandr.local>
2025-11-21 14:17:43 +0300

    Установил Async Storage. Реализовал запись и загрузку истории в/из хранилища

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
-import { createContext, useState, useCallback } from 'react';
+import { createContext, useState, useCallback, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { MoodOptionType, MoodOptionWithTimestamp } from '../src/types.ts';
+
+const storageKey = 'my-app-data';
+
+type AppData = {
+  moods: MoodOptionWithTimestamp[];
+};
+
+const getAppData = async (): Promise<AppData | null> => {
+  try {
+    const data = await AsyncStorage.getItem(storageKey);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+    return null;
+  } catch {
+    return null;
+  }
+};
+
+const setAppData = async (newData: AppData) => {
+  try {
+    await AsyncStorage.setItem(storageKey, JSON.stringify(newData));
+  } catch {}
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -21,8 +47,23 @@ type AppProviderProps = {
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
+  useEffect(() => {
+    const getDataFromStorage = async () => {
+      const data = await getAppData();
+
+      if (data) {
+        setMoodList(data.moods);
+      }
+    };
+    getDataFromStorage();
+  }, []);
+
   const handleSelectMood = useCallback((mood: MoodOptionType) => {
-    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+    setMoodList(current => {
+      const newValue = [...current, { mood, timestamp: Date.now() }];
+      setAppData({ moods: newValue });
+      return newValue;
+    });
   }, []);
 
   return (
