77a956b1ff9b481bfef614ca4109ee80c2a24e06
Alexandr <alex@local>
2026-07-15 12:35:20 +0300

    chore(deps): remove unused dependencies
    - Uninstalled react-native-paper, react-native/new-app-screen and react-native-gesture-handler.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { BottomTabsNavigator } from './screens/BottomTabs.navigator.tsx';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,12 +13,10 @@ export const App: React.FC = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaProvider>
-          <GestureHandlerRootView style={styles.container}>
-            <NavigationContainer>
-              <BottomTabsNavigator />
-            </NavigationContainer>
-          </GestureHandlerRootView>
+        <SafeAreaProvider style={styles.container}>
+          <NavigationContainer>
+            <BottomTabsNavigator />
+          </NavigationContainer>
         </SafeAreaProvider>
       </PersistGate>
     </Provider>
@@ -31,8 +29,3 @@ const styles = StyleSheet.create({
   },
 });
 //TODO: SelfTracker новое имя
-//TODO: лишние зависимости
-//npm uninstall
-// "react-native-paper": "^5.15.2",
-// "react-native-linear-gradient": "^2.8.3",
-// "@react-native/new-app-screen": "0.81.4",
